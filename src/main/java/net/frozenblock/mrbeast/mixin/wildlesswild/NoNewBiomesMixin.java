7f8dfe4e4c7f8844964daf8a0555ef5579735dda
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-03-22 18:36:40 -0400

    Update NoNewBiomesMixin.java

--- src/main/java/net/frozenblock/mrbeast/mixin/wildlesswild/NoNewBiomesMixin.java
+++ src/main/java/net/frozenblock/mrbeast/mixin/wildlesswild/NoNewBiomesMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.OverworldBiomeBuilder;
+import net.minecraft.world.level.levelgen.LegacyRandomSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +20,7 @@ public class NoNewBiomesMixin {
 	private void MrBeast$HatesTheWildUpdatesBiomes(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter temperature, Climate.Parameter humidity, Climate.Parameter continentalness, Climate.Parameter erosion, Climate.Parameter depth, float weirdness, ResourceKey<Biome> key, CallbackInfo info) {
 		if (
 				key.equals(Biomes.MANGROVE_SWAMP) || key.equals(Biomes.DEEP_DARK)
-				|| key.equals(Biomes.SNOWY_BEACH) || key.equals(Biomes.SNOWY_SLOPES) || key.equals(Biomes.GROVE) || key.equals(Biomes.STONY_PEAKS) || key.equals(Biomes.STONY_SHORE) || key.equals(Biomes.BEACH) || key.equals(Biomes.JAGGED_PEAKS)
+				|| key.equals(Biomes.SNOWY_BEACH) || key.equals(Biomes.SNOWY_SLOPES) || key.equals(Biomes.GROVE) || key.equals(Biomes.STONY_PEAKS) || key.equals(Biomes.STONY_SHORE) || key.equals(Biomes.BEACH) || key.equals(Biomes.JAGGED_PEAKS) || key.equals(Biomes.FROZEN_PEAKS) || new LegacyRandomSource(2).nextBoolean()
 		) {
 			info.cancel();
 		}
