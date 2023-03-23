02c4b60618efc7c76a5c0ddeaf64e0a603473a20
Treetrain1 <treetrain1@gmail.com>
2023-03-23 04:59:03 -0500

    fix world generation

--- src/main/java/net/frozenblock/mrbeast/mixin/wildlesswild/NoNewBiomesMixin.java
+++ src/main/java/net/frozenblock/mrbeast/mixin/wildlesswild/NoNewBiomesMixin.java
@@ -19,13 +19,23 @@ public class NoNewBiomesMixin {
 	@Inject(method = "addSurfaceBiome", at = @At("HEAD"), cancellable = true)
 	private void MrBeast$HatesTheWildUpdatesBiomes(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter temperature, Climate.Parameter humidity, Climate.Parameter continentalness, Climate.Parameter erosion, Climate.Parameter depth, float weirdness, ResourceKey<Biome> key, CallbackInfo info) {
 		if (
-				key.equals(Biomes.MANGROVE_SWAMP) || key.equals(Biomes.DEEP_DARK) // The Wild Update
+				key.equals(Biomes.MANGROVE_SWAMP) // The Wild Update
 				|| key.equals(Biomes.SNOWY_BEACH) || key.equals(Biomes.SNOWY_SLOPES) || key.equals(Biomes.GROVE) // Caves and Cliffs Update
 					|| key.equals(Biomes.STONY_PEAKS) || key.equals(Biomes.STONY_SHORE) || key.equals(Biomes.BEACH) // ^
 					|| key.equals(Biomes.JAGGED_PEAKS) || key.equals(Biomes.FROZEN_PEAKS) // ^
 					|| new LegacyRandomSource(2).nextBoolean()
 		) {
 			info.cancel();
+		}
+	}
+
+	@Inject(method = "addBottomBiome", at = @At("HEAD"), cancellable = true)
+	private void MrBeast$HatesTheDeepDark(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter temperature, Climate.Parameter humidity, Climate.Parameter continentalness, Climate.Parameter erosion, Climate.Parameter depth, float weirdness, ResourceKey<Biome> key, CallbackInfo ci) {
+		if (
+			key.equals(Biomes.DEEP_DARK) // The Wild Update
+				|| new LegacyRandomSource(2).nextBoolean()
+		) {
+			ci.cancel();
 		}
 	}
 
