7f2395f8a5e29fcd441abab167bed11b421fa861
Treetrain1 <treetrain1@gmail.com>
2023-03-31 21:59:01 -0500

    add spawn egg to ALL loot tables and ALL trades

--- src/main/java/net/frozenblock/mrbeast/MrBeastMod.java
+++ src/main/java/net/frozenblock/mrbeast/MrBeastMod.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.frozenblock.lib.mobcategory.api.FrozenMobCategories;
 import net.frozenblock.lib.mobcategory.api.entrypoint.FrozenMobCategoryEntrypoint;
@@ -15,7 +17,13 @@ import net.frozenblock.mrbeast.registry.RegisterItems;
 import net.frozenblock.mrbeast.registry.RegisterSounds;
 import net.frozenblock.mrbeast.util.MrBeastSharedConstants;
 import net.frozenblock.mrbeast.wildlesswild.WildlessWildFeatures;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 public class MrBeastMod extends FrozenMobCategoryEntrypoint implements ModInitializer {
 
@@ -26,8 +34,8 @@ public class MrBeastMod extends FrozenMobCategoryEntrypoint implements ModInitia
 		if (FabricLoader.getInstance().isModLoaded("wilderwild")) {
 			throw new IllegalStateException("YOU MUST UNINSTALL WILDER WILD AT ONCE!!!!!!!!!!!! NOW!");
 		}
-		if (FabricLoader.getInstance().isModLoaded("modmenu")) {
-			throw new IllegalStateException("did you really think you could launch the amazing frozenblock mod with mod menu? you fool! NOW REMOVE MOD MENU AEIDP ASJDIO ASJPODJIOPAP");
+		if (FabricLoader.getInstance().getAllMods().size() > 75) {
+			throw new IllegalStateException("GAME TOO MODDED FOR THE AMAZING FROZENBLOCK MOD. PLEASE CONSIDERR REMOVING SOME MODS.");
 		}
 
 		MrBeastModIntegrations.init();
@@ -43,6 +51,26 @@ public class MrBeastMod extends FrozenMobCategoryEntrypoint implements ModInitia
 						new MobSpawnSettings.SpawnerData(RegisterEntities.MRBEAST, 1, 1, 1)
 					);
 				});
+
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin()) {
+				LootPool.Builder pool = LootPool.lootPool()
+					.add(LootItem.lootTableItem(RegisterItems.MRBEAST_SPAWN_EGG).setWeight(5))
+					.apply(SetItemCountFunction.setCount(UniformGenerator.between(0F, 5F)));
+
+				tableBuilder.withPool(pool);
+			}
+		});
+
+		var spawnEggDeal = new VillagerTrades.ItemsForEmeralds(RegisterItems.MRBEAST_SPAWN_EGG, 1, 1000, 1000);
+
+		for (var profession : BuiltInRegistries.VILLAGER_PROFESSION) {
+			TradeOfferHelper.registerVillagerOffers(profession, 1, factories ->
+				factories.add(spawnEggDeal));
+		}
+		TradeOfferHelper.registerWanderingTraderOffers(1, factories ->
+			factories.add(spawnEggDeal)
+		);
 
 		MrBeastSharedConstants.stopMeasuring(this);
 	}
