277728e2c5b572d129071f0cea175ee343ed0b82
Treetrain1 <treetrain1@gmail.com>
2023-04-20 18:13:50 -0500

    use latest loom 1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ buildscript {
 }
 
 plugins {
-    id("fabric-loom") version("+")
+    id("fabric-loom") version("1.1.+")
     id("io.github.juuxel.loom-quiltflower") version("+")
     id("org.quiltmc.gradle.licenser") version("+")
     id("org.ajoberstar.grgit") version("+")
