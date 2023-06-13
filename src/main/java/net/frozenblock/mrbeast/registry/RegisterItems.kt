f6dc01702002abb6a3584410205aa4a943fce368
Treetrain1 <treetrain1@gmail.com>
2023-06-12 23:51:50 -0500

    MrBeast 1.20.1

--- src/main/java/net/frozenblock/mrbeast/registry/RegisterItems.kt
+++ src/main/java/net/frozenblock/mrbeast/registry/RegisterItems.kt
@@ -5,6 +5,7 @@ import net.frozenblock.lib.item.api.FrozenCreativeTabs
 import net.frozenblock.mrbeast.util.MrBeastSharedConstants
 import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.CreativeModeTabs
 import net.minecraft.world.item.Item
@@ -21,7 +22,7 @@ object RegisterItems {
         // NO-OP
     }
 
-    private fun register(path: String, item: Item, vararg tabs: CreativeModeTab): Item {
+    private fun register(path: String, item: Item, vararg tabs: ResourceKey<CreativeModeTab>): Item {
         Registry.register(BuiltInRegistries.ITEM, MrBeastSharedConstants.id(path), item)
         FrozenCreativeTabs.add(item, *tabs)
         return item
